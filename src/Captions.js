f268a477dc9477c4b6b6f2860eaf557dab165052
RPIUdishan <it18082234@my.sliit.lk>
2021-02-01 16:13:09 +0530

    2nd commoit

--- src/Captions.js
+++ src/Captions.js
@@ -1,37 +1,22 @@
 import React from "react";
-import {captionData} from './data';
 import '../node_modules/video-react/dist/video-react.css'
-// import "node_modules/video-react/dist/video-react.css";
 import { Player } from 'video-react';
 
+import sub from './sub.vtt'
 export const Captions = () => {
     return (
-        // <div className="caption-container">
-    
-
       <div className="caption-container">
-      <Player
-textTrack = {captionData}
-      
-      src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
-      
-    />
-        {/* {captionData.map((data, key) => {
-          return (
-            <div key={key}>
-              {data.text +
-                " , " +
-                data.startTime +
-                " ," +
-                data.endTime }
-            </div>
-          );
-        })} */}
-        
+      <Player videoId="video-1" autoPlay>
+      <source
+        src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
+        type="video/mp4"
+      />
+      <track
+        kind="captions"
+        src={sub}
+        default
+      />
+      </Player>
       </div>
-
-
-    
-        // </div>
     );
 }
